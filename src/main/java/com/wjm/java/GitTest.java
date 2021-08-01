e7727285df588c147ae8c817def7173189b6acc6
wjm <wjm@123.com>
2021-08-01 10:02:55 +0800

    hot-fix first git commit

--- src/main/java/com/wjm/java/GitTest.java
+++ src/main/java/com/wjm/java/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("git test is success");
         System.out.println("git test is second success");
+        System.out.println("git test is third success");
     }
 }
