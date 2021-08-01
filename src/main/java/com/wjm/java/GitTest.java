7764cdb29b904ee37546c30c97b73e622e9e0c5f
wjm <wjm@123.com>
2021-08-01 09:57:24 +0800

    second git commit

--- src/main/java/com/wjm/java/GitTest.java
+++ src/main/java/com/wjm/java/GitTest.java
@@ -3,5 +3,6 @@ package com.wjm.java;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("git test is success");
+        System.out.println("git test is second success");
     }
 }
